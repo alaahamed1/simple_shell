dfc7332fe493ceccbe0aef0dcbe93f9d8ec3bb9c
SondosEssam <sondosessam713@gmail.com>
2024-02-28 23:04:52 +0200

    the head of everthing

--- header.h
+++ header.h
@@ -9,21 +9,19 @@
 #include <sys/wait.h>
 int isExit(char *c);
 int countwords(char *str, char *d);
-void free_fun(char **tmp, int i, char *tmp2);
 char **spilt_string(char *delim, char *str);
-int check_the_slash(char *str);
-int check_the_spaces(char *buff);
-void check_state(int is_cmplte, char **argv, char *file, char **env);
-void check_if_true(char *pathname, char **argv, char *filename);
 char *make_path_arr(char **str);
+int check_the_slash(char *str);
 char *compare_and_set_env(char **env, char **argv);
 void free_grid(char **grid);
 int excute_function(char **argv, char *str, char *filename);
 void print_env(char **envp);
 void display_dollar_sign(void);
-void display_prompet(char **env, char **envp, char **arg);
-void non_interactive_mode(char **env, char **arg, char **envp);
+void display_prompet(char **env, char **envp, char **arg, int count);
+void non_interactive_mode(char **env, char **arg, char **envp, int count);
+void check_if_true(char *pathname, char **argv, int count, char *filename);
 int _getline(char **line_ptr, size_t *n, FILE *stream);
-
-
+int check_the_spaces(char *buff);
+void free_fun(char **tmp, int i, char *tmp2);
+void check_state(int is_cmplte, char **argv, int cnt, char *file, char **env);
 #endif /* START_H */
